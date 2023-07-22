aaf840139911930dce951b270291e9994cef3840
shubham122334 <shubham1999.iimt@gmail.com>
2023-07-22 11:07:56 +0530

    showing recipe in index page

--- src/main/java/com/shuham/springrecipeapp/controller/IndexController.java
+++ src/main/java/com/shuham/springrecipeapp/controller/IndexController.java
@@ -1,15 +1,23 @@
 package com.shuham.springrecipeapp.controller;
 
+import com.shuham.springrecipeapp.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
 
-    @RequestMapping({"","/","/index"})
-    public String getIndexPage(){
+    private final RecipeService recipeService;
 
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(Model model){
+        model.addAttribute("recipes",recipeService.getRecipes());
         return "views/index";
     }
 }
